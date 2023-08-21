6b4c386758d50349d7faa6c9254b15a0ca6f0344
MuhdHanish <muhammedhanish11@gmail.com>
2023-08-21 15:12:33 +0530

    changed the time to local time

--- src/framework/database/models/MessageModel.ts
+++ src/framework/database/models/MessageModel.ts
@@ -6,7 +6,14 @@ export type MongoDBMessage = Model<Document<any, any, any> & Message>;
 const messageSchema = new Schema<Message>({
   sender: { type: mongoose.Types.ObjectId, ref: "Ngo" },
   content: { type: String, trim: true },
-  time: { type: Date, default: Date.now },
+  time: {
+    type: Date,
+    default: () => {
+      const now = new Date();
+      const localTime = new Date(now.getTime() - now.getTimezoneOffset() * 60000);
+      return localTime;
+    }
+  }
 });
 
 export const messageModel: MongoDBMessage = mongoose.connection.model<
